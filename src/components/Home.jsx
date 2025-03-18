ad568f127798e179a9b9e646ac8d6ec6680fe511
eslam-mohamedl <ee.eslam355@gmail.com>
2025-03-18 11:45:19 +0200

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import Landing from './Landing'
+function Home() {
+  return (
+    <Fragment>
+        <Landing />
+        </Fragment>
+  )
+}
+
+export default Home
---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Landing() {
+  return (
+   <section>
+    <div className="conatiner">
+        <div>
+           <img src="/src/assets/illustration-intro.png" alt="landing-img" />
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default Landing
