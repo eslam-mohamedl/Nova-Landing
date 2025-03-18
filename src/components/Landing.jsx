fc7c1c88bc4f95107cdf773711b1975fcc3cbb44
eslam-mohamedl <ee.eslam355@gmail.com>
2025-03-18 11:57:33 +0200

    second commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,7 +5,7 @@ function Landing() {
    <section>
     <div className="conatiner">
         <div>
-           <img src="/public/assets/images/illustration-intro.png" alt="landing-img" />
+           <img src="../../public/assets/images/illustration-intro.png" alt="landing-img" />
         </div>
     </div>
    </section>
